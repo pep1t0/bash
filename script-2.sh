fbfa4344e720816b6cc8ef882ba1a53751d558ef
Pep1t0 <daniel.requena@aol.com>
2022-09-25 14:13:04 +0200

    1

---
+++ script-2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+var="8dm7KsjU28B7v621Jls"
+value="ERmFRMVZ0U2paTlJYTkxDZz09Cg"
+
+for i in {1..40}
+do
+        var=$(echo $var | base64)
+
+        # Se hacen dos comparaciones: 
+        # 1. Si el contenido de value esta dentro de var
+        # 2. Si el numero de caracteres es superior a 113450
+        #        
+		if [[ `echo $var | grep $value` && $(echo $var | wc -c) > 113450 ]]
+        then
+            echo $var | tail -c 20
+        fi
+done
